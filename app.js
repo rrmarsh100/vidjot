a78ed059d391f626eaaad3ffbfa98639916a0aab
Robert Marsh <nhe6dc@gmail.com>
2018-05-09 13:24:49 -0400

    body parser and error mes sages added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser')
 const app = express();
 
 //Map global promise - get rid of warning
@@ -21,6 +21,10 @@ const Idea = mongoose.model('ideas');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 // Index Route
 app.get('/', (req, res) => {
   const title = 'Welcome1';
@@ -37,6 +41,27 @@ app.get('/about', (req, res) => {
 //add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+// Process From
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if(!req.body.title){
+    errors.push({text: 'Please add a title'})
+  }
+  if(!req.body.details){
+    errors.push({text: 'Please add some details'})
+  }
+  if(errors.length > 0){
+    res.render('ideas/add', {
+      errors: errors, 
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else{
+    res.send('passed');
+  }
 });
 
 const port = 5000;
