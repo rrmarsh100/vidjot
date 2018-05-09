bb4c29886aefc889d766d2b86e3eeccceb131cc5
Robert Marsh <nhe6dc@gmail.com>
2018-05-09 12:53:10 -0400

    Add Video Idea Skele Setup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
 
 const app = express();
+
 //Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
 // Connect to mongoose
@@ -11,6 +12,10 @@ mongoose.connect('mongodb://localhost/vidjot-dev', {
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
+
+//Load Idea Model
+require('./models/Idea')
+const Idea = mongoose.model('ideas');
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -27,6 +32,11 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+//add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
 });
 
 const port = 5000;
