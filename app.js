6867fb77967ced30d076f0d2fcc917ce5d55c860
Robert Marsh <nhe6dc@gmail.com>
2018-05-09 12:26:20 -0400

    Mongoose Working

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+//Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+  useMongoClient: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
