ec2c7d644a4de3ba36c19a47188943a6e00f0551
Robert Marsh <nhe6dc@gmail.com>
2018-05-12 11:02:06 -0400

    fixed db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://<Bobby>:<Bobby>@ds119370.mlab.com:19370/vidjotprod'}
+  module.exports = {mongoURI: 'mongodb://Bobby:Bobby@ds119370.mlab.com:19370/vidjotprod'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
